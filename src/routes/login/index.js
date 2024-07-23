10fcbcbe95cc9a16521bab9d37a647fb7296fba5
ajhmohammed <50343026+ajhmohammed@users.noreply.github.com>
2024-07-23 23:21:49 +0530

    Changed logo, updated input field style, changed alert style and position and added backdrop

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,5 +1,5 @@
 import { LoadingButton } from '@mui/lab';
-import { Alert, Snackbar, TextField } from '@mui/material';
+import { Alert, Snackbar, TextField, Backdrop, CircularProgress, Button, FormControl } from '@mui/material';
 import { useRef, useState } from 'react';
 import { API_URL } from '../../constants';
 import './styles.css';
@@ -10,24 +10,44 @@ const Login = () => {
     const emailRef = useRef(null);
     const passwordRef = useRef(null);
 
+    const [open, setOpen] = useState(false);
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
     return <div className='login-container'>
+
+    <div>
+      {/* <Button onClick={handleOpen}>Show backdrop</Button> */}
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        // onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </div>
         <img src='/logo512.png' className='login-img' alt='Logo' />
         <h1 className='title'>Beneficiary Portal</h1>
-        <div className='login-form'>
-            <div className='input-row'>
-                <label>Email/Mobile</label>
-                <TextField placeholder='Email/Mobile' type="email" size='small' inputRef={emailRef} />
+        <FormControl className='login-form'>
+            <div className='input-row help-centered'>
+                <TextField type="email" id="standard-basic" size='small' label="Email / Mobile" variant="standard" inputRef={emailRef}/>
             </div>
-            <div className='input-row'>
-                <label>Password</label>
-                <TextField placeholder='Password' size='small' type='password' inputRef={passwordRef} />
+            <br/>
+            <div className='input-row help-centered'>
+                <TextField type="password" id="standard-basic" size='small' label="Password" variant="standard" inputRef={passwordRef}/>
             </div>
             <div className='login-btn'>
-                <LoadingButton variant="outlined" loading={isLoading} size="small" onClick={async () => {
+                <LoadingButton variant="contained" loading={isLoading} onClick={async () => {
                     setIsLoading(true);
                     if (emailRef.current.value && passwordRef.current.value) {
+
                         var mailformat = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(.\w{2,3})+$/;
                         var mobileformat = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
                         if (emailRef.current.value.match(mailformat) || emailRef.current.value.match(mobileformat)) {
                             const out = await (await fetch(`${await API_URL()}pub/login`, {
                                 method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({
@@ -42,6 +62,7 @@ const Login = () => {
                                 document.cookie = `dhis_token=${out.data.token};`;
                                 localStorage.setItem('dhis_user',JSON.stringify(out.data.user));
                                 setTimeout(()=>window.location.href = '/',2000);
+                                handleOpen();
                             } else {
                                 setMessage({ text: out.errorMessage, status: 400 });
                             }
@@ -54,14 +75,27 @@ const Login = () => {
                     setIsLoading(false);
                 }}>Login</LoadingButton>
             </div>
-        </div>
-        <Snackbar open={message} autoHideDuration={3000} onClose={() => setMessage(null)}>
-            <Alert onClose={() => setMessage(null)} severity={message && message.status === 200 ? "success" : "error"} sx={{ width: '100%' }}>
+        </FormControl>
+        <Snackbar 
+            anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center"
+            }} 
+            open={message} 
+            autoHideDuration={3000} 
+            onClose={
+                () => setMessage(null)
+            }
+        >
+
+            <Alert variant="filled" onClose={() => setMessage(null)} severity={message && message.status === 200 ? "success" : "error"} sx={{ width: '100%' }}>
                 {message ? message.text : ''}
             </Alert>
+
         </Snackbar>
         {/* <a className='link' href='/loginAdmin'>Admin Portal</a> */}
-        <a style={{ marginTop: 10 }} className='link' href='/register'>Register</a>
+        <p>Do not have an account?</p>
+        <Button variant="outlined" style={{ marginTop: 10 }} className='link' href='/register'>Register</Button>
     </div>
 }
 
